b05f979be84fea8554decc7d62828d4787ebc7fd
Julius Tens <mail@juliustens.eu>
2018-09-03 17:36:28 +0200

    rewrite tests using tape

---
+++ test/simple.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const chart = require('..')
+const tape = require('tape')
+
+const expected = require('./data/simple.json')
+
+const parliament = {
+	headBench: {
+		speaker: {
+			seats: 3,
+			colour: '#abc'
+		}
+	},
+	left: {
+		labour: {
+			seats: 200,
+			colour: '#d21'
+		},
+		snp: {
+			seats: 30,
+			colour: '#ff0'
+		}
+	},
+	crossBench: {
+		dup: {
+			seats: 30,
+			colour: '#d64'
+		}
+	},
+	right: {
+		conservative: {
+			seats: 200,
+			colour: '#08d'
+		},
+        libdems: {
+            seats: 60,
+            colour: '#ff0'
+        }
+	}
+}
+
+tape('Simple example', t => {
+    const svg = JSON.parse(JSON.stringify(chart(parliament)))
+	t.deepEqual(svg, expected)
+	t.end()
+})
---
+++ test/uk-2017.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const chart = require('..')
+const tape = require('tape')
+
+const expected = require('./data/uk-2017.json')
+
+const parliament = {
+	headBench: {
+		speaker: {
+			seats: 1,
+			colour: '#000'
+		}
+	},
+	left: {
+		labour: {
+			seats: 262,
+			colour: '#dc241f'
+		},
+		snp: {
+			seats: 35,
+			colour: '#ff0'
+		},
+		libdems: {
+			seats: 12,
+			colour: '#faa61a'
+		},
+		sinnfein: {
+			seats: 7,
+			colour: '#080'
+		},
+		plaidcymru: {
+			seats: 4,
+			colour: '#008142'
+		},
+		green: {
+			seats: 1,
+			colour: '#6ab023'
+		},
+		independent: {
+			seats: 1,
+			colour: '#aadfff'
+		}
+	},
+	crossBench: {
+		dup: {
+			seats: 10,
+			colour: '#d46a4c'
+		}
+	},
+	right: {
+		conservative: {
+			seats: 317,
+			colour: '#0087dc'
+		}
+	}
+}
+
+tape('2017 UK general election', t => {
+    const svg = JSON.parse(JSON.stringify(chart(parliament)))
+	t.deepEqual(svg, expected)
+	t.end()
+})
