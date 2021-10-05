2e986d21f8468e2918aad8e8db532606b25dea85
Julius Tens <mail@juliustens.eu>
2021-10-05 11:59:03 +0200

    housekeeping: add linter and depcheck, remove unused dependencies

--- test/uk-2017.js
+++ test/uk-2017.js
@@ -9,55 +9,55 @@ const parliament = {
 	headBench: {
 		speaker: {
 			seats: 1,
-			colour: '#000'
-		}
+			colour: '#000',
+		},
 	},
 	left: {
 		labour: {
 			seats: 262,
-			colour: '#dc241f'
+			colour: '#dc241f',
 		},
 		snp: {
 			seats: 35,
-			colour: '#ff0'
+			colour: '#ff0',
 		},
 		libdems: {
 			seats: 12,
-			colour: '#faa61a'
+			colour: '#faa61a',
 		},
 		sinnfein: {
 			seats: 7,
-			colour: '#080'
+			colour: '#080',
 		},
 		plaidcymru: {
 			seats: 4,
-			colour: '#008142'
+			colour: '#008142',
 		},
 		green: {
 			seats: 1,
-			colour: '#6ab023'
+			colour: '#6ab023',
 		},
 		independent: {
 			seats: 1,
-			colour: '#aadfff'
-		}
+			colour: '#aadfff',
+		},
 	},
 	crossBench: {
 		dup: {
 			seats: 10,
-			colour: '#d46a4c'
-		}
+			colour: '#d46a4c',
+		},
 	},
 	right: {
 		conservative: {
 			seats: 317,
-			colour: '#0087dc'
-		}
-	}
+			colour: '#0087dc',
+		},
+	},
 }
 
 tape('2017 UK general election', t => {
-    const svg = JSON.parse(JSON.stringify(chart(parliament)))
+	const svg = JSON.parse(JSON.stringify(chart(parliament)))
 	t.deepEqual(svg, expected)
 	t.end()
 })
